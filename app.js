7fcd5e9355206b3ca933c2132b4d7fce3346802a
TeamAbella060420 <jabella@rscm.com.sa>
2022-05-15 15:06:50 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-var people = ["Kenneth", "Alena", "Andrew", "Alex", "June", "Treasure"];
-var person = people[1];
+// var people = ["Kenneth", "Alena", "Andrew", "Alex", "June", "Treasure"];
+// var person = people[3];
 
-console.log("person")
+// console.log(person)
+var janet;
+janet.prototype = Person("Janet", 18);
+
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+
+
+console.log(janet)
